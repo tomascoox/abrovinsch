3fca89842836c9e1b2c72b7cbf4cf731ad9c9a3a
Tomas Coox <tomas@joox.se>
2023-12-07 12:06:06 +0100

    Add CSS variable for viewport height

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+'use client'
+
 import Image from 'next/image'
 import styles from './Home.module.css'
+import { useEffect } from 'react'
 
 export default function Home() {
+    useEffect(() => {
+        // Set a CSS variable with the actual viewport height
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }, []);
+
     return (
-        <main className="flex h-screen w-full flex-col items-center justify-center p-4 bg-white">
+        <main className="flex h-screen w-full flex-col items-center justify-center p-4 bg-white" style={{ height: 'calc(var(--vh, 1vh) * 100)' }}>
             {/* Logo container */}
             <div className="flex h-full w-full items-center justify-center">
                 <div className={styles.logo + " w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6"}>
