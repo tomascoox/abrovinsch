8d27545ffad0795b9e384955ea000d374a739f91
Tomas Coox <tomas@joox.se>
2023-12-07 12:01:17 +0100

    Add animation to logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image'
+import styles from './Home.module.css'
 
 export default function Home() {
     return (
         <main className="flex h-screen w-full flex-col items-center justify-center p-4 bg-white">
             {/* Logo container */}
             <div className="flex h-full w-full items-center justify-center">
-                <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6">
-                    {' '}
-                    {/* Responsive container */}
+                <div className={styles.logo + " w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6"}>
                     <Image
                         src="/abrovinsch-logotype-color.svg" // Replace with your logo path
                         alt="Abrovinsch Logo"
